27cbc62592687c8e313556c5f5115b070888c49d
Alejandro Rodriguez <alex98rodriguezromero@gmail.com>
2025-04-20 19:33:49 -0500

    Añade documentación con Swagger en la API
    Se implementó Swagger para generar documentación interactiva de la API. Esto incluye detalles sobre los endpoints públicos y privados utilizados por el cliente web y servicios internos. Ahora está disponible en la ruta '/docs'.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Navark API')
+    .setDescription(
+      'API oficial de Navark, un juego multijugador de batalla naval. Gestiona usuarios, partidas, rankings, equipos y lógica de juego en tiempo real. Esta documentación cubre los endpoints públicos y privados utilizados por el cliente web y servicios internos.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
