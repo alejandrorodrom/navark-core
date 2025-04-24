212abf73e2c772fb40852af70678b0c46fe3ec2f
Alejandro Rodriguez <alex98rodriguezromero@gmail.com>
2025-04-23 19:55:16 -0500

    Refactoriza definición de UserRepository en UserModule
    Se reemplaza el string 'UserRepository' por la clase UserRepository para mejorar la tipificación y consistencia en el módulo. Esto asegura una mejor integración y reduce posibles errores en tiempo de ejecución.

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { PrismaUserRepository } from './infrastructure/prisma/user.prisma.repository';
 import { BcryptPasswordService } from './infrastructure/bcrypt/bcrypt-password.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { UserRepository } from './domain/repository/user.repository';
 
 @Module({
   imports: [PrismaModule],
   controllers: [],
   providers: [
     {
-      provide: 'UserRepository',
+      provide: UserRepository,
       useClass: PrismaUserRepository,
     },
     BcryptPasswordService,
   ],
-  exports: ['UserRepository', BcryptPasswordService],
+  exports: [UserRepository, BcryptPasswordService],
 })
 export class UserModule {}
