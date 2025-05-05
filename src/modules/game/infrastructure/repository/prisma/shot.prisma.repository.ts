b480cc2af6f2f59e424fe3fa97ca667e044ed59e
Alejandro Rodriguez <alex98rodriguezromero@gmail.com>
2025-05-04 22:21:34 -0500

    Refactoriza importación del modelo Shot en repositorio
    Se actualizó la ruta del modelo Shot para utilizar un archivo de tipos centralizado. Esto mejora la organización y reduce el acoplamiento en las dependencias del repositorio.

--- src/modules/game/infrastructure/repository/prisma/shot.prisma.repository.ts
+++ src/modules/game/infrastructure/repository/prisma/shot.prisma.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../../../prisma/prisma.service';
 import { ShotRepository } from '../../../domain/repository/shot.repository';
-import { Shot } from 'prisma-client-f15084464449711e4caee4566fa960144a1bc91a54a7ba08fcf8d12e47ec9ee3';
+import { Shot } from '../../../../../prisma/prisma.types';
 import { ShotTarget, ShotType } from '../../../domain/models/shot.model';
 
 @Injectable()
